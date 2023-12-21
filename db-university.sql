68760faa2c2ad4e2d7fb6161ce32a3e66a6e19e8
valerioperria <valerio.perria@gmail.com>
2023-12-21 15:11:11 +0100

    feat. Group_by

--- db-university.sql
+++ db-university.sql
@@ -55,3 +55,31 @@ WHERE `phone` IS NULL;
 SELECT COUNT(*)
 FROM `teachers`
 WHERE `phone` IS NULL;
+
+
+--GROUP_BY------------------------------------------------------------------------
+
+-- 1. Contare quanti iscritti ci sono stati ogni anno
+
+SELECT COUNT(*) as `num_students`, YEAR(`enrolment_date`) as `enrolment_date`
+FROM `students`
+GROUP BY YEAR(`enrolment_date`);
+
+-- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
+
+SELECT COUNT(*) as `num_teachers`, `office_address`
+FROM `teachers`
+GROUP BY `office_address`;
+
+-- 3. Calcolare la media dei voti di ogni appello d'esame
+
+SELECT `exam_id`, AVG(`vote`) as `media_voti`
+FROM `exam_student`
+GROUP BY `exam_id`;
+
+-- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
+
+SELECT COUNT(*) as `num_corsi`, `department_id`
+FROM `degrees`
+GROUP BY `department_id`;
+
